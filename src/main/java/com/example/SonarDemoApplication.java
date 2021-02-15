1a0c7ff710806d631f5de8a3f30dafd077d7a7c5
Sudheer <rskumar780@gmail.com>
2021-02-15 11:32:19 +0530

    Initial version

---
+++ src/main/java/com/example/SonarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentServiceTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.model.Student;
+import com.example.service.IStudentService;
+
+
+@SpringBootTest
+//@Transactional
+class StudentServiceTest {
+	@Autowired
+	IStudentService studentService;
+	@Test
+	void testAddStudent() {
+		Student student = new Student("Sam", "Sam@gmail.com");
+		Student std = studentService.addStudent(student);
+		assertEquals("Sam", std.getName());
+	}
+	@Test
+	void testGetStudent() {
+		Student std = studentService.getStudent(41l);
+		assertNotNull(std);
+	}
+	
+	@Test
+	void testPrintStudent() {
+		Student student = new Student("Kumar", "Kumar@gmail.com");
+		Student std = studentService.addStudent(student);
+		assertEquals(student, std);
+	}
+}
